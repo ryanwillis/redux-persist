63d7feab38aa6b9105d09dadd254aa1ab69641e4
Ryan Willis <technosis@gmail.com>
2021-07-30 16:55:22 -0700

    Replaced rollup-plugin-uglify with rollup-plugin-terser

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@
 import nodeResolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
 import replace from '@rollup/plugin-replace';
-import { uglify } from 'rollup-plugin-uglify';
+import { terser } from 'rollup-plugin-terser';
 
 const env = process.env.NODE_ENV
 const config = {
@@ -32,12 +32,12 @@ config.plugins.forEach(plugin => {
 
 if (env === 'production') {
   config.plugins.push(
-    uglify({
+    terser({
       compress: {
         pure_getters: true,
         unsafe: true,
         unsafe_comps: true
-      },
+      }
     })
   )
 }
